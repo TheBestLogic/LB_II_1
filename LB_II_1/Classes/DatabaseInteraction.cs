61f8c06d19ff2b07aca38a18568fdf163b4cd706
TheBestLogic <alex310595@mail.ru>
2016-10-09 21:37:03 +0300

    Еще немного правок и начало работы с БД

--- LB_II_1/Classes/DatabaseInteraction.cs
+++ LB_II_1/Classes/DatabaseInteraction.cs
@@ -9,5 +9,29 @@ namespace LB_II_1.Classes
 {
     class DatabaseInteraction
     {
+        //connectionString="Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projects\VisualStudio\LB_II_1\LB_II_1\Database\Database.mdf;Integrated Security=True"
+        public int CreateConn()
+        {
+            
+            return 0;
+        }
+
+        public int GetTable()
+        {
+
+            return 0;
+        }
+
+        public int SaveTable()
+        {
+
+            return 0;
+        }
+
+        public int LostConn()
+        {
+
+            return 0;
+        }
     }
 }
